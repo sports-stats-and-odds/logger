6338529bf7314e11e8de7099c60f27570b753023
Matteo DE VITA PC <matteo.de-vita@epitech.eu>
2022-11-30 15:44:56 +0100

    refacto logger package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='logger',
     version='1.0',
-    packages=['logger.py'],
+    packages=['logger'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     url='https://github.com/sports-stats-and-odds/logger',
